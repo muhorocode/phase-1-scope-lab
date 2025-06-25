fe6e7c056f880adcb10e10e357c1053a7a246ae8
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-06-25 07:07:40 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName="bob"
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase()
+    return customerName
+}
+
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer="james"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='eliud'
+}
